0a12dd4117c45ee62feaeef1090dcb6faf6a8c8e
Yordan Saldaña <yordan.saldana@medisprout.com>
2024-06-18 10:42:55 -0300

    create Proyect

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface AppProps {
+  message: string;
+}
+
+const App: React.FC<AppProps> = ({ message }) => {
+  return <div>{message}</div>;
+};
+
+export default App;
